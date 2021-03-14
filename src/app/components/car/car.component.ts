b3f8b85eab3569cce951c563a8eeb734d1c084c6
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-14 20:46:29 +0300

    Done homework.(10.03.2021-17.Day)

---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/car';
+import { CarService } from 'src/app/services/car.service';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  dataLoaded = false;
+  cars:Car[] = [];
+  constructor(private carService:CarService) { }
+
+  ngOnInit(): void {
+    this.getCars();
+  }
+
+  getCars(){
+    this.carService.getCars().subscribe(response=>{
+      this.cars = response.data; 
+      this.dataLoaded = true;
+    });
+  }
+
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,7 @@
+export interface Rental{
+    id:number,
+    brandName:string,
+    userFullName:string,
+    rentDate:Date,
+    returnDate:Date,
+}
