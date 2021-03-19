146f48e60c53ca39048862210f0d46ae5458d2bf
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-03-19 16:21:40 +0300

    added new methods and refactored.

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -9,12 +9,27 @@ import { ListResponseModel } from '../models/listResponseModel';
 })
 export class CarService {
 
-  apiUrl = 'https://localhost:44339/api/cars/getdetails';
+  apiUrl = 'https://localhost:44339/api/';
   constructor(private httpClient:HttpClient) { }
 
   getCars():Observable<ListResponseModel<Car>>{
-    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl);
+    let newPath = this.apiUrl + "cars/getdetails";
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
   }
 
+  getCarsByBrandId(id:number):Observable<ListResponseModel<Car>>{
+    let newPath = this.apiUrl+ "cars/getdetailsbybrandid?id="+id;
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
+
+  getCarsByColorId(id:number):Observable<ListResponseModel<Car>>{
+    let newPath = this.apiUrl + "cars/getdetailsbycolorid?id="+id;
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
+
+  getCarDetailsByCarId(carId:number):Observable<ListResponseModel<Car>>{
+    let newPath = this.apiUrl + "cars/getdetailsbycarid?id="+carId;
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
 
 }
