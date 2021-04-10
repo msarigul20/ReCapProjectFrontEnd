7c40f97a9da320213f3b43fac78b84786116728d
Mustafa SARIGUL <m.sarigul199@gmail.com>
2021-04-10 16:52:42 +0300

    added new models.

---
+++ src/app/models/customer-detail.ts
@@ -0,0 +1,9 @@
+export interface CustomerDetail{
+    customerId:number;
+    companyName:string
+    firstName:string
+    lastName:string
+    email:string
+    password:string
+
+}
---
+++ src/app/models/rental-detail.ts
@@ -0,0 +1,9 @@
+export interface RentalDetail{
+    id?: number,
+    carId: number,
+    brandName: string,
+    colorName: string,
+    customerId:number,
+    rentDate: Date,
+    returnDate: Date
+}
